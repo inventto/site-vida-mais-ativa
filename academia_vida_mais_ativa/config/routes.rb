24946f2f06966e3da45b1b2f3e0202b9c1e1af7c
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2014-09-17 08:43:29 -0300

    Adicionado a Gem Koala.

--- academia_vida_mais_ativa/config/routes.rb
+++ academia_vida_mais_ativa/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   mount Refinery::Core::Engine, at: '/'
 
 
+  get '/fotos' => 'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
