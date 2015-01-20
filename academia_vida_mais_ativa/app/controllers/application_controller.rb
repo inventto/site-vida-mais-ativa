6109a9c86f3e0086d38fb67a31f764efbdd46aa1
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2015-01-20 14:08:52 -0200

    Retirado a gem Koala. Adiconado a meta tag do google.

--- academia_vida_mais_ativa/app/controllers/application_controller.rb
+++ academia_vida_mais_ativa/app/controllers/application_controller.rb
@@ -1,17 +1,8 @@
 class ApplicationController < ActionController::Base
-  require 'koala'
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def index
-    @graph = Koala::Facebook::GraphAPI.new(access_token)
-    # 1.2 and later
-    @graph = Koala::Facebook::API.new(access_token)
-    @graph.put_picture(file, content_type, facebook_arguments = {}, user_or_album_id, http_options = {})
-    # so for instance
-    @graph.put_picture(file, content_type, {:message => "My upload message"}, "me")
-    @graph.put_picture(params[:file], {:message => "Message"}, my_album_id)
   end
-
 end
