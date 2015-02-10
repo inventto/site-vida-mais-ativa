04e46ceace049723ee94b236709137048201496a
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2015-02-10 10:58:40 -0200

    Alterado o domain.

--- academia_vida_mais_ativa/config/initializers/mail.rb
+++ academia_vida_mais_ativa/config/initializers/mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '25',
-  :domain => 'localhost:3000',
+  :domain => 'vidamaisativa.com',
   :authentication => :plain,
   :user_name => Rails.application.secrets.sendgrid_username,
   :password => Rails.application.secrets.sendgrid_password
